02f3919aee27ee7e28ef30f31b6b8037b90b52d5
ilostmywing <1912980339@qq.com>
2023-01-03 21:38:16 +0800

    第一次传

--- 前端作业/作业3/第3次作业.js
+++ 前端作业/作业3/第3次作业.js
@@ -1,13 +1,32 @@
-function pus(arr1,addl)
-{
-let oldl=arr1.length;
-let arr2=new Array(old1+addl);
-arr2=arr1;
-for (let i = arr1.length; i < arr2.length+1; i++) 
-{
-    arr2[i] =prompt();    
+// function pus(arr1,addl)
+// {
+// let oldl=arr1.length;
+// let arr2=new Array(old1+addl);
+// arr2=arr1;
+// for (let i = arr1.length; i < arr2.length+1; i++) 
+// {
+//     arr2[i] =prompt();    
+// }
+// return arr2;
+// }
+// let arr1=[1];
+// console.pus(arr1,2);
+let obj1= new Object();
+obj1.name="luck";
+obj1.age="996";
+console.log(obj1);
+
+let obj2 ={
+    name:'hello',
+    age:'world'
 }
-return arr2;
+console.log(obj2);
+function Person(name,age)
+{
+    this.name = name;
+    this.age = age;
+    this.sayname = () => {
+        console.log(this.name)
+    }
 }
-let arr1=[1];
-console.pus(arr1,2);
+const p1=new Person('libai',20)
